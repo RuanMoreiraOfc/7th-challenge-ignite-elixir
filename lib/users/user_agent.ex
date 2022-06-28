684050f8400bf4b15504e650f720382bebe85391
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-06-28 11:36:24 -0300

    feat: create `get/1` for `User.Agent`

--- lib/users/user_agent.ex
+++ lib/users/user_agent.ex
@@ -7,10 +7,19 @@ defmodule Flightex.Users.Agent do
     Agent.start_link(fn -> initial_state end, name: __MODULE__)
   end
 
+  def get(cpf), do: Agent.get(__MODULE__, &get_user(&1, cpf))
+
   def save(%{cpf: cpf} = user) do
     Agent.update(__MODULE__, &update_state(&1, user, cpf))
 
     {:ok, user}
+  end
+
+  defp get_user(state, cpf) do
+    case Map.get(state, cpf) do
+      nil -> {:error, "User not found"}
+      user -> {:ok, user}
+    end
   end
 
   defp update_state(oldState, %User{} = user, cpf) do
