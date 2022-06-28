9c66f0d7e1b097ee2831cdb2190cb455a9cf21b0
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-06-28 10:28:42 -0300

    feat: build `Booking` from params

--- lib/bookings/booking.ex
+++ lib/bookings/booking.ex
@@ -3,7 +3,15 @@ defmodule Flightex.Bookings.Booking do
   @enforce_keys @keys
   defstruct @keys
 
-  def build do
-    # TO DO
+  def build(complete_date, local_origin, local_destination, user_id) do
+    response = %__MODULE__{
+      complete_date: complete_date,
+      local_origin: local_origin,
+      local_destination: local_destination,
+      user_id: user_id,
+      id: UUID.uuid4()
+    }
+
+    {:ok, response}
   end
 end
